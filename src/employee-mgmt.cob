734726ac747f26329ff1317cc70e8cfcd6c20049
Sisamkele022 <aphindakwakho@gmail.com>
2025-04-24 14:10:32 +0200

    Divivded the compilation of te sub systems first then the main system

--- src/employee-mgmt.cob
+++ src/employee-mgmt.cob
@@ -1,177 +1,146 @@
+      *> Free format COBOL program
        IDENTIFICATION DIVISION.
        PROGRAM-ID. EMPLOYEE-MGMT.
-       AUTHOR.     BYTEBANK-DEV.
-      *****************************************************************
-      *****************************************************************
-       ENVIRONMENT DIVISION. 
-       INPUT-OUTPUT SECTION. 
-       FILE-CONTROL. 
+       AUTHOR. BYTEBANK-DEV.
+
+       ENVIRONMENT DIVISION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
            SELECT EMPLOYEE-FILE ASSIGN TO "data/employees.dat"
               ORGANIZATION IS INDEXED
               ACCESS MODE IS DYNAMIC
               RECORD KEY IS EMP-ID
-              FILE STATUS IS FILE-STATUS-CODE
-              .
-      *****************************************************************
-      *****************************************************************
+              FILE STATUS IS FILE-STATUS-CODE.
+
        DATA DIVISION.
-       FILE SECTION. 
+       FILE SECTION.
        FD EMPLOYEE-FILE.
        01 EMPLOYEE-RECORD.
-           05  EMP-ID                    PIC 9(10).
-           05  EMP-NAME                  PIC X(50).
-           05  EMP-SURNAME               PIC X(50).
-           05  EMP-POSITION-TYPE         PIC X(20).
-           05  EMP-BIRTH               .
-              10 EMP-BIRTH-YEAR             PIC X(4).
-              10 EMP-BIRTH-MONTH            PIC X(2).
-              10 EMP-BIRTH-DATE             PIC X(2).
-
-           05  EMP-AGE                   PIC 9(2).
-      *****   THIS IS AN OPTIONAL DATA-ITEM    ****
-           05 EMP-UNION-FEE              PIC 9(5).
+           05 EMP-ID                    PIC 9(10).
+           05 EMP-NAME                  PIC X(50).
+           05 EMP-SURNAME               PIC X(50).
+           05 EMP-POSITION-TYPE         PIC X(20).
+           05 EMP-BIRTH.
+              10 EMP-BIRTH-YEAR         PIC X(4).
+              10 EMP-BIRTH-MONTH        PIC X(2).
+              10 EMP-BIRTH-DATE         PIC X(2).
+           05 EMP-AGE                   PIC 9(2).
+           05 EMP-UNION-FEE             PIC 9(5).
 
        WORKING-STORAGE SECTION.
-      *****DATA ITEMS FOR INPUTTING AND WRITING TO FILE
        01 WS-EMPLOYEE.
-           05  WS-EMP-ID                       PIC 9(10).
-           05  WS-EMP-NAME                     PIC X(50).
-           05  WS-EMP-SURNAME                  PIC X(50).
-           05  WS-EMP-POSITION-TYPE            PIC X(20).
+           05 WS-EMP-ID                 PIC 9(10).
+           05 WS-EMP-NAME               PIC X(50).
+           05 WS-EMP-SURNAME            PIC X(50).
+           05 WS-EMP-POSITION-TYPE      PIC X(20).
+           05 WS-EMP-BIRTH.
+              10 WS-EMP-BIRTH-YEAR      PIC X(4).
+              10 WS-EMP-BIRTH-MONTH     PIC X(2).
+              10 WS-EMP-BIRTH-DATE      PIC X(2).
+           05 WS-EMP-AGE                PIC 9(2).
+           05 WS-EMP-UNION-FEE          PIC 9(5).
 
-      * THE EMPLOYEE'S FULL BIRTHDATE     
-           05  WS-EMP-BIRTH               .
-              10 WS-EMP-BIRTH-YEAR             PIC X(4).
-              10 WS-EMP-BIRTH-MONTH            PIC X(2).
-              10 WS-EMP-BIRTH-DATE             PIC X(2).
-
-           05  WS-EMP-AGE                      PIC 9(2).
-      *****   THIS IS AN OPTIONAL DATA-ITEM    ****
-           05  WS-EMP-UNION-FEE                PIC 9(5).
-
-       77 WS-EOF             PIC X(1) VALUE 'N'.
-       77 USER-INPUT         PIC X(1).
-       77 FILE-STATUS-CODE   PIC X(2).
-       77 FILE-EXISTS        PIC X(1) VALUE 'N'.
-       01 WS-LOW-KEY         PIC 9(10) VALUE ZEROS.
+       77 WS-EOF                        PIC X(1) VALUE 'N'.
+       77 USER-INPUT                    PIC X(1).
+       77 FILE-STATUS-CODE              PIC X(2).
+       77 FILE-EXISTS                   PIC X(1) VALUE 'N'.
+       01 WS-LOW-KEY                    PIC 9(10) VALUE ZEROS.
 
        PROCEDURE DIVISION.
        BEGIN.
            PERFORM CHECK-FILE-EXISTS
            IF FILE-EXISTS = 'N'
               PERFORM CREATE-EMPLOYEE-LIST
-           END-IF.
+           END-IF
 
            PERFORM UNTIL USER-INPUT = "4"
-           DISPLAY "========== EMPLOYEE MANAGEMENT MODULE ============"
-           DISPLAY " "
-           DISPLAY "1. LIST ALL EMPLOYEES"
-           DISPLAY "2. ADD A NEW EMPLOYEE"
-           DISPLAY "3. SEARCH EMPLOYEE BY ID"
-           DISPLAY " "
-           DISPLAY "4. RETURN TO MAIN MENU"
-           DISPLAY " "
-           DISPLAY "CHOICE: " WITH NO ADVANCING 
-           ACCEPT USER-INPUT
+              DISPLAY "========== EMPLOYEE MANAGEMENT MODULE ============"
+              DISPLAY " "
+              DISPLAY "1. LIST ALL EMPLOYEES"
+              DISPLAY "2. ADD A NEW EMPLOYEE"
+              DISPLAY "3. SEARCH EMPLOYEE BY ID"
+              DISPLAY " "
+              DISPLAY "4. RETURN TO MAIN MENU"
+              DISPLAY " "
+              DISPLAY "CHOICE: " WITH NO ADVANCING 
+              ACCEPT USER-INPUT
 
-           EVALUATE USER-INPUT
-             WHEN "1"
-              PERFORM LIST-ALL-EMPLOYEES
-             
-             WHEN "2"
-              PERFORM ADD-NEW-EMPLOYEE
-
-             WHEN "3"
-              PERFORM SEARCH-BY-ID
-
-      * ALLOWS TO RETURN BACK TO THE CALLING PROGRAM (i.e, MAIN MENU)
-             WHEN "4"
-             STOP RUN
-
-             WHEN OTHER 
-             DISPLAY "INVALID INPUT"
-           END-EVALUATE
-
+              EVALUATE USER-INPUT
+                WHEN "1"
+                  PERFORM LIST-ALL-EMPLOYEES
+                WHEN "2"
+                  PERFORM ADD-NEW-EMPLOYEE
+                WHEN "3"
+                  PERFORM SEARCH-BY-ID
+                WHEN "4"
+                  EXIT PROGRAM
+                WHEN OTHER 
+                  DISPLAY "INVALID INPUT"
+              END-EVALUATE
            END-PERFORM
-           .
-      *****************************************************************
-      *****************************************************************
+           GOBACK.
+
        CHECK-FILE-EXISTS.
            OPEN INPUT EMPLOYEE-FILE
-
            IF FILE-STATUS-CODE = "00"
               CLOSE EMPLOYEE-FILE 
               MOVE 'Y' TO FILE-EXISTS
            ELSE
               MOVE 'N' TO FILE-EXISTS
-           END-IF
-           .
-      *****************************************************************
-      *****************************************************************
+           END-IF.
+
        CREATE-EMPLOYEE-LIST.
-      * OUTPUT CREATES FILE OR OVERWRITES EXISTING
-      * HERE, IT IS DETERMINED FILE DOESN'T EXIST
-      * SO IT IS SAFE TO USE OUTPUT
            OPEN OUTPUT EMPLOYEE-FILE
            DISPLAY "CREATING FILE"
-
            IF FILE-STATUS-CODE = "00"
               DISPLAY "FILE CREATED. code: " FILE-STATUS-CODE
-           CLOSE EMPLOYEE-FILE
+              CLOSE EMPLOYEE-FILE
            ELSE
               DISPLAY "ERROR CREATING FILE. code: " FILE-STATUS-CODE
-           END-IF
-           .
-      *****************************************************************
-      *****************************************************************
+           END-IF.
+
        LIST-ALL-EMPLOYEES.
            OPEN I-O EMPLOYEE-FILE
-           IF FILE-STATUS-CODE = "00" OR FILE-STATUS-CODE = "97"
-              DISPLAY " "
-              DISPLAY "BYTEBANK EMPLOYEES"
-              DISPLAY " "
+           IF FILE-STATUS-CODE NOT = "00" AND FILE-STATUS-CODE NOT = "97"
+              DISPLAY "ERROR READING EMPLOYEES LIST. code: " FILE-STATUS-CODE
+              GOBACK
+           END-IF
+
+           DISPLAY " "
+           DISPLAY "BYTEBANK EMPLOYEES"
+           DISPLAY " "
            
-      * TELLS THE FILE POINTER WHERE THE START OF THE FILE IS
            MOVE WS-LOW-KEY TO EMP-ID
            START EMPLOYEE-FILE KEY >= EMP-ID
                INVALID KEY 
                    DISPLAY "NO RECORDS FOUND"
                    MOVE 'Y' TO WS-EOF
-               NOT INVALID KEY 
-                   CONTINUE
            END-START
              
            PERFORM UNTIL WS-EOF = 'Y'
-           READ EMPLOYEE-FILE NEXT RECORD 
-              AT END MOVE 'Y' TO WS-EOF
-              DISPLAY " "
-              DISPLAY "||||||||||||||||||||||||||||||||||||||||||||||"
-              DISPLAY "                END OF FILE"
-              DISPLAY "||||||||||||||||||||||||||||||||||||||||||||||"
-              DISPLAY " "
-              NOT AT END
-                 DISPLAY " "
-                 DISPLAY "============================================="
-                 DISPLAY "ID: " EMP-ID
-                 DISPLAY "NAME: " EMP-NAME
-                 DISPLAY "SURNAME: " EMP-SURNAME
-                 DISPLAY "POSITION: " EMP-POSITION-TYPE
-                 DISPLAY "BIRTHDATE: " EMP-BIRTH-DATE
-                 DISPLAY "AGE: " EMP-AGE
-                 DISPLAY "UNION FEE: " EMP-UNION-FEE
-                 DISPLAY "============================================="
-           END-READ
-           CLOSE EMPLOYEE-FILE
+              READ EMPLOYEE-FILE NEXT RECORD 
+                 AT END 
+                    MOVE 'Y' TO WS-EOF
+                    DISPLAY " "
+                    DISPLAY "||||||||||||||||||||||||||||||||||||||||||||||"
+                    DISPLAY "                END OF FILE"
+                    DISPLAY "||||||||||||||||||||||||||||||||||||||||||||||"
+                    DISPLAY " "
+                 NOT AT END
+                    DISPLAY " "
+                    DISPLAY "============================================="
+                    DISPLAY "ID: " EMP-ID
+                    DISPLAY "NAME: " EMP-NAME
+                    DISPLAY "SURNAME: " EMP-SURNAME
+                    DISPLAY "POSITION: " EMP-POSITION-TYPE
+                    DISPLAY "BIRTHDATE: " EMP-BIRTH-DATE
+                    DISPLAY "AGE: " EMP-AGE
+                    DISPLAY "UNION FEE: " EMP-UNION-FEE
+                    DISPLAY "============================================="
+              END-READ
            END-PERFORM
-           
-           ELSE
-              DISPLAY "ERROR READING EMPLOYEES LIST. code: "
-                 FILE-STATUS-CODE
-              GOBACK
-           END-IF
-           .
-      *****************************************************************
-      *****************************************************************
+           CLOSE EMPLOYEE-FILE.
+
        ADD-NEW-EMPLOYEE.
            OPEN I-O EMPLOYEE-FILE
            DISPLAY "ENTER EMPLOYEE'S FIRST NAME: " WITH NO ADVANCING 
@@ -181,53 +150,43 @@
            ACCEPT WS-EMP-SURNAME
 
            DISPLAY "ENTER EMPLOYEE'S POSITION TYPE (INTERN, "
-           "INTERMEDIATE, SENIOR): " WITH NO ADVANCING 
+              "INTERMEDIATE, SENIOR): " WITH NO ADVANCING 
            ACCEPT WS-EMP-POSITION-TYPE
 
-           DISPLAY "ENTER EMPLOYEE'S BIRTH YEAR (YYYY): " 
-              WITH NO ADVANCING   
+           DISPLAY "ENTER EMPLOYEE'S BIRTH YEAR (YYYY): " WITH NO ADVANCING   
            ACCEPT WS-EMP-BIRTH-YEAR
 
-           DISPLAY "ENTER EMPLOYEE'S BIRTH MONTH (MM): " 
-              WITH NO ADVANCING 
+           DISPLAY "ENTER EMPLOYEE'S BIRTH MONTH (MM): " WITH NO ADVANCING 
            ACCEPT WS-EMP-BIRTH-MONTH
 
-           DISPLAY "ENTER EMPLOYEE'S BIRTH DATE (DD): " 
-              WITH NO ADVANCING 
+           DISPLAY "ENTER EMPLOYEE'S BIRTH DATE (DD): " WITH NO ADVANCING 
            ACCEPT WS-EMP-BIRTH-DATE
 
            DISPLAY "ENTER EMPLOYEE'S AGE: " WITH NO ADVANCING 
            ACCEPT WS-EMP-AGE
 
-           DISPLAY "(OPTIONAL) ENTER EMPLOYEE'S UNION FEE: "
-            WITH NO ADVANCING 
+           DISPLAY "(OPTIONAL) ENTER EMPLOYEE'S UNION FEE: " WITH NO ADVANCING 
            ACCEPT WS-EMP-UNION-FEE
 
-           MOVE WS-EMP-NAME              TO EMP-NAME
-           MOVE WS-EMP-SURNAME           TO EMP-SURNAME
-           MOVE WS-EMP-POSITION-TYPE     TO EMP-POSITION-TYPE
-           MOVE WS-EMP-BIRTH-YEAR        TO EMP-BIRTH-YEAR
-           MOVE WS-EMP-BIRTH-MONTH       TO EMP-BIRTH-MONTH
-           MOVE WS-EMP-BIRTH-DATE        TO EMP-BIRTH-DATE
-           MOVE WS-EMP-AGE               TO EMP-AGE
-           MOVE WS-EMP-UNION-FEE         TO EMP-UNION-FEE
+           MOVE WS-EMP-NAME TO EMP-NAME
+           MOVE WS-EMP-SURNAME TO EMP-SURNAME
+           MOVE WS-EMP-POSITION-TYPE TO EMP-POSITION-TYPE
+           MOVE WS-EMP-BIRTH-YEAR TO EMP-BIRTH-YEAR
+           MOVE WS-EMP-BIRTH-MONTH TO EMP-BIRTH-MONTH
+           MOVE WS-EMP-BIRTH-DATE TO EMP-BIRTH-DATE
+           MOVE WS-EMP-AGE TO EMP-AGE
+           MOVE WS-EMP-UNION-FEE TO EMP-UNION-FEE
 
            WRITE EMPLOYEE-RECORD
               INVALID KEY
                  DISPLAY WS-EMP-ID
                  DISPLAY WS-EMP-NAME " ALREADY EXISTS!"
-
               NOT INVALID KEY
-              DISPLAY EMP-NAME " HAS BEEN SUCCESSFULLY ADDED."
+                 DISPLAY EMP-NAME " HAS BEEN SUCCESSFULLY ADDED."
            END-WRITE
-           CLOSE EMPLOYEE-FILE
-           GOBACK
-           .
-      *****************************************************************
-      *****************************************************************
-       SEARCH-BY-ID.
+           CLOSE EMPLOYEE-FILE.
 
-      * INITIALISING VARIABLES FOR SEARCH AND DISPLAY
+       SEARCH-BY-ID.
            INITIALIZE WS-EMP-ID
            DISPLAY "ID: " WS-EMP-ID
            INITIALIZE WS-EMP-NAME
@@ -242,21 +201,22 @@
            DISPLAY "                     SEARCH                        "
            DISPLAY "==================================================="
 
-      * CHECK IF FILE HAS BEEN OPENED CORRECTLY
-           IF FILE-STATUS-CODE = "00" OR FILE-STATUS-CODE = "97"
-              DISPLAY " "
-              DISPLAY "ENTER EMPLOYEE ID: " WITH NO ADVANCING 
-              ACCEPT WS-EMP-ID
+           IF FILE-STATUS-CODE NOT = "00" AND FILE-STATUS-CODE NOT = "97"
+              DISPLAY "ERROR READING FILE. code: " FILE-STATUS-CODE
+              CLOSE EMPLOYEE-FILE
+              GOBACK
+           END-IF
 
-              MOVE WS-EMP-ID TO EMP-ID
+           DISPLAY "ENTER EMPLOYEE ID: " WITH NO ADVANCING 
+           ACCEPT WS-EMP-ID
 
-              READ EMPLOYEE-FILE INTO EMPLOYEE-RECORD
-                 KEY IS EMP-ID
+           MOVE WS-EMP-ID TO EMP-ID
 
-                 INVALID KEY
-                    DISPLAY "EMPLOYEE NOT FOUND!"
-
-                 NOT INVALID KEY 
+           READ EMPLOYEE-FILE INTO EMPLOYEE-RECORD
+              KEY IS EMP-ID
+              INVALID KEY
+                 DISPLAY "EMPLOYEE NOT FOUND!"
+              NOT INVALID KEY 
                  DISPLAY " "
                  DISPLAY "============================================="
                  DISPLAY "ID: " EMP-ID
@@ -268,10 +228,7 @@
                  DISPLAY "UNION FEE: " EMP-UNION-FEE
                  DISPLAY "============================================="
                  DISPLAY " "
-              END-READ
-           ELSE
-              DISPLAY "ERROR READING FILE. code: " FILE-STATUS-CODE
-           END-IF 
-           CLOSE EMPLOYEE-FILE
-           .
+           END-READ
+           CLOSE EMPLOYEE-FILE.
+
        END PROGRAM EMPLOYEE-MGMT.
